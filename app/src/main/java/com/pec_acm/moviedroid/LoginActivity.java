893f474e8828f8f713dfb91c6fe36c83cad63d88
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-02-06 17:00:27 +0530

    Fixing stuff :heh:

--- app/src/main/java/com/pec_acm/moviedroid/LoginActivity.java
+++ app/src/main/java/com/pec_acm/moviedroid/LoginActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -37,7 +38,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase_auth);
-
         SignInButton signInButton = findViewById(R.id.sign_in_button);
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
