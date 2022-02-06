7302c94fcd0a7f9296caf806214881b85276a023
kavyarishi19 <forcodingpurposes19@gmail.com>
2022-02-06 16:11:00 +0530

    Login added

--- app/src/main/java/com/pec_acm/moviedroid/MainActivity.java
+++ app/src/main/java/com/pec_acm/moviedroid/MainActivity.java
@@ -2,13 +2,79 @@ package com.pec_acm.moviedroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    private static final String ARG_NAME = "username";
+    public static void startActivity(Context context, String username) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(ARG_NAME, username);
+        context.startActivity(intent);
+    }
+    FirebaseAuth firebaseAuth;
+    GoogleSignInClient googleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.textViewWelcome);
+        if (getIntent().hasExtra(ARG_NAME)) {
+            textView.setText(String.format("Welcome - %s", getIntent().getStringExtra(ARG_NAME)));
+        }
+        findViewById(R.id.buttonLogout).setOnClickListener(this);
+        findViewById(R.id.buttonDisconnect).setOnClickListener(this);
+        googleSignInClient = GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_SIGN_IN);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonLogout:
+                signOut();
+                break;
+            case R.id.buttonDisconnect:
+                revokeAccess();
+                break;
+        }
+    }
+    private void signOut() {
+        // Firebase sign out
+        firebaseAuth.signOut();
+        // Google sign out
+        googleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // Google Sign In failed, update UI appropriately
+                        Log.w(TAG, "Signed out of google");
+                    }
+                });
+    }
+    private void revokeAccess() {
+        // Firebase sign out
+        firebaseAuth.signOut();
+        // Google revoke access
+        googleSignInClient.revokeAccess().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // Google Sign In failed, update UI appropriately
+                        Log.w(TAG, "Revoked Access");
+                    }
+                });
     }
 }
