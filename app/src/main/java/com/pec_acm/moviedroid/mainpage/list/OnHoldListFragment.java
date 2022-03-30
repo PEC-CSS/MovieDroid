4a3a531925b01efc884c25fca5844f08105acc85
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-03-30 16:25:35 +0530

    Unnecessary code in fragments removed and small changes

--- app/src/main/java/com/pec_acm/moviedroid/mainpage/list/OnHoldListFragment.java
+++ app/src/main/java/com/pec_acm/moviedroid/mainpage/list/OnHoldListFragment.java
@@ -10,52 +10,7 @@ import android.view.ViewGroup;
 
 import com.pec_acm.moviedroid.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OnHoldListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class OnHoldListFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public OnHoldListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OnHoldListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OnHoldListFragment newInstance(String param1, String param2) {
-        OnHoldListFragment fragment = new OnHoldListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
