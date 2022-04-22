a9f4770d81288e4c933622070742057177a9fb8c
Raveesh2324 <raveeshgoyal2504@gmail.com>
2022-04-22 14:46:36 +0530

    added viewbinding in SearchFragment

--- app/src/main/java/com/pec_acm/moviedroid/mainpage/search/SearchFragment.kt
+++ app/src/main/java/com/pec_acm/moviedroid/mainpage/search/SearchFragment.kt
@@ -5,16 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.pec_acm.moviedroid.R
+import com.pec_acm.moviedroid.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+    private var _binding: FragmentSearchBinding? = null
+    val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        _binding = FragmentSearchBinding.inflate(layoutInflater,container,false)
+        val view = binding.root
+        return view
     }
 
 }
