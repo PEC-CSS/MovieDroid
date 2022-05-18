b4f90b9d20c2395aba1a8b23db87545cdea85770
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-05-18 20:50:23 +0530

    connected details frags to all item frag

--- app/src/main/java/com/pec_acm/moviedroid/mainpage/adapters/NormalAdapter.kt
+++ app/src/main/java/com/pec_acm/moviedroid/mainpage/adapters/NormalAdapter.kt
@@ -6,17 +6,32 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.pec_acm.moviedroid.R
 import com.pec_acm.moviedroid.firebase.ListItem
+import com.pec_acm.moviedroid.mainpage.home.AllItemsDirections
 
 
 class NormalAdapter(private val images:List<ListItem>, val context: Context): RecyclerView.Adapter<NormalAdapter.ViewHolder>() {
-    class ViewHolder(itemView : View): RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView : View, var itemCategory: String? = null, var itemID: Int? = null): RecyclerView.ViewHolder(itemView) {
         val image: ImageView = itemView.findViewById(R.id.imginwa)
         val movieTitle: TextView = itemView.findViewById(R.id.movie_title)
-
+        init {
+            itemView.setOnClickListener {
+                itemID?.let {
+                    if (itemCategory == "movie") {
+                        val direction =
+                            AllItemsDirections.actionAllItemsToMovieDetailFragment(it)
+                        itemView.findNavController().navigate(direction)
+                    } else if (itemCategory == "tv") {
+                        itemView.findNavController()
+                            .navigate(AllItemsDirections.actionAllItemsToTvDetailFragment(it))
+                    }
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -28,6 +43,8 @@ class NormalAdapter(private val images:List<ListItem>, val context: Context): Re
         val image = images[position]
         Glide.with(context).load(image.posterUrl).into(holder.image)
         holder.movieTitle.text = image.name
+        holder.itemID = image.id
+        holder.itemCategory = image.category
     }
 
     override fun getItemCount(): Int {
