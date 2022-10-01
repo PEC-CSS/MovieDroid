6fb168b8ef2029c0b5326249b9c60277ca544863
vishal2376 <vishalsingh2376@gmail.com>
2022-10-01 20:47:13 +0530

    profile activity added

---
+++ app/src/main/java/com/pec_acm/moviedroid/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.pec_acm.moviedroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
