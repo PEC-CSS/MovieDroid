7122cc217a039d0af834168b73c385387ec5b871
shivamparihar12 <parihar.shivam0789@gmail.com>
2022-10-01 15:45:52 +0530

    PlanToWatchListFragment migrated to kotlin

--- app/src/main/java/com/pec_acm/moviedroid/mainpage/list/PlanToWatchListFragment.kt
+++ app/src/main/java/com/pec_acm/moviedroid/mainpage/list/PlanToWatchListFragment.kt
@@ -1,24 +1,15 @@
 package com.pec_acm.moviedroid.mainpage.list
 
-import androidx.lifecycle.ViewModelProvider.get
-import com.pec_acm.moviedroid.mainpage.list.ListViewModel.getUser
-import com.pec_acm.moviedroid.mainpage.list.ListViewModel.user
-import com.pec_acm.moviedroid.firebase.User.userList
-import com.pec_acm.moviedroid.firebase.ListItem.status
-import com.pec_acm.moviedroid.mainpage.list.ListAdapter.setItemList
-import com.pec_acm.moviedroid.mainpage.list.ListViewModel
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.RecyclerView
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.pec_acm.moviedroid.databinding.FragmentPlanToWatchListBinding
 import com.pec_acm.moviedroid.firebase.ListItem
-import com.pec_acm.moviedroid.firebase.User
 import java.util.ArrayList
 
 class PlanToWatchListFragment : Fragment() {
