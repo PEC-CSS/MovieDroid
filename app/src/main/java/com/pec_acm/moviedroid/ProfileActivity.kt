190898a067e63d5baac3dec9069fd6bae3914d06
vishal2376 <vishalsingh2376@gmail.com>
2022-10-02 12:32:22 +0530

    binding implemented

--- app/src/main/java/com/pec_acm/moviedroid/ProfileActivity.kt
+++ app/src/main/java/com/pec_acm/moviedroid/ProfileActivity.kt
@@ -2,10 +2,16 @@ package com.pec_acm.moviedroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pec_acm.moviedroid.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
