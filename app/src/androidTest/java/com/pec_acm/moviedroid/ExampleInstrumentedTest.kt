51e067dd8032ecd9f228cb5790c1c4f4fd098fb0
krishnachaitanya0107 <krishnachaitanya010700@gmail.com>
2022-10-04 23:15:25 +0530

    Converted the remaining java files to kotlin

---
+++ app/src/androidTest/java/com/pec_acm/moviedroid/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.pec_acm.moviedroid
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        Assert.assertEquals("com.pec_acm.moviedroid", appContext.packageName)
+    }
+
+}
