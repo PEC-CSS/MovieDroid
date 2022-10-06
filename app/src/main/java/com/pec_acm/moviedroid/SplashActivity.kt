8403b6337403dbf497719c20dda3b8f38fe54171
karanpreet8082 <karan2440zaildar@gmail.com>
2022-10-06 12:00:01 +0530

    Added splash screen

---
+++ app/src/main/java/com/pec_acm/moviedroid/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.pec_acm.moviedroid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
+    }
+}
