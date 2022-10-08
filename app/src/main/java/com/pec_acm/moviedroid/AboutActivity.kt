1753730aaba3edb11cdf39b3be08a69303545c3e
vishal2376 <vishalsingh2376@gmail.com>
2022-10-08 18:36:26 +0530

    binding added

--- app/src/main/java/com/pec_acm/moviedroid/AboutActivity.kt
+++ app/src/main/java/com/pec_acm/moviedroid/AboutActivity.kt
@@ -2,10 +2,17 @@ package com.pec_acm.moviedroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pec_acm.moviedroid.databinding.ActivityAboutBinding
 
 class AboutActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityAboutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
+
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
