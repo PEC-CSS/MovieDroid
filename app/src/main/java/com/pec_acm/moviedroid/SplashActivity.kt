d435d666b42eaca97651c20ead6a17d225eaf9a9
Debashish Kundu <debashish.joy@gmail.com>
2022-10-23 00:03:21 +0530

    reverted back to gradle 7.0.1

--- app/src/main/java/com/pec_acm/moviedroid/SplashActivity.kt
+++ app/src/main/java/com/pec_acm/moviedroid/SplashActivity.kt
@@ -4,12 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@SplashActivity,LoginActivity::class.java)
             startActivity(intent)
             finish()
