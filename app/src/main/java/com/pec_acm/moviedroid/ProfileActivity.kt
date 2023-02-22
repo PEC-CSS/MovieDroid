c77cc34749d3790c1c124d4ed654e998389cf021
armaan <armaanbadhan07@gmail.com>
2023-02-22 18:02:51 +0530

    profileactivity piechart ui complete

--- app/src/main/java/com/pec_acm/moviedroid/ProfileActivity.kt
+++ app/src/main/java/com/pec_acm/moviedroid/ProfileActivity.kt
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
+import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.firebase.auth.FirebaseAuth
 import com.pec_acm.moviedroid.databinding.ActivityProfileBinding
 import com.pec_acm.moviedroid.mainpage.profile.ProfileViewModel
@@ -28,37 +30,53 @@ class ProfileActivity : AppCompatActivity() {
 
         val user = FirebaseAuth.getInstance().currentUser
         profileViewModel.setUserRatingValues(user!!.uid)
-        //update the info
 
         // https://github.com/PhilJay/MPAndroidChart/blob/master/MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/PieChartActivity.java
         binding.pieChart.isDrawHoleEnabled = true
+        binding.pieChart.setHoleColor(Color.TRANSPARENT)
+        binding.pieChart.holeRadius = 35f
+        binding.pieChart.transparentCircleRadius = 40f
         binding.pieChart.setUsePercentValues(true)
-        binding.pieChart.setEntryLabelTextSize(15f)
-        binding.pieChart.setEntryLabelColor(Color.BLACK)
-        binding.pieChart.centerText = "text in middle?"
-        binding.pieChart.setCenterTextSize(24f)
+        binding.pieChart.setEntryLabelTextSize(13.5f)
+        binding.pieChart.setEntryLabelColor(Color.WHITE)
         binding.pieChart.description.isEnabled = false
-        binding.pieChart.legend.isEnabled = false
+        binding.pieChart.legend.verticalAlignment = Legend.LegendVerticalAlignment.CENTER
+        binding.pieChart.legend.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
+        binding.pieChart.legend.orientation = Legend.LegendOrientation.VERTICAL
+        binding.pieChart.legend.setDrawInside(false)
+        binding.pieChart.legend.isEnabled = true
+        binding.pieChart.legend.textColor = Color.WHITE
+        binding.pieChart.legend.textSize = 15f
+        // legend.setCustom(List<LegendEntry>)
 
 
-        var entries = ArrayList<PieEntry>()
-        entries.add(PieEntry(0.2f, "asdf"))
-        entries.add(PieEntry(0.15f, "qwer"))
-        entries.add(PieEntry(0.10f, "zxcv"))
-        entries.add(PieEntry(0.25f, "poiu"))
-        entries.add(PieEntry(0.3f, "lkjh"))
+        val entries = ArrayList<PieEntry>()
+        entries.add(PieEntry(0.2f, resources.getString(R.string.watching_status)))
+        entries.add(PieEntry(0.15f, resources.getString(R.string.completed_status)))
+        entries.add(PieEntry(0.10f, resources.getString(R.string.on_hold_status)))
+        entries.add(PieEntry(0.25f, resources.getString(R.string.dropped_status)))
+        entries.add(PieEntry(0.3f, resources.getString(R.string.plan_to_watch_status)))
 
-        var dataset = PieDataSet(entries, "title maibi")
-        var data = PieData(dataset)
+        val colors = ArrayList<Int>()
+        colors.add(resources.getColor(R.color.green))
+        colors.add(resources.getColor(R.color.purple_700))
+        colors.add(resources.getColor(R.color.yellow))
+        colors.add(resources.getColor(R.color.red))
+        colors.add(resources.getColor(R.color.grey))
+
+        val dataset = PieDataSet(entries, "")
+        dataset.colors = colors
+        val data = PieData(dataset)
         data.setDrawValues(true)
         data.setValueFormatter(PercentFormatter(binding.pieChart))
-        data.setValueTextSize(12f)
-        data.setValueTextColor(Color.BLACK)
+        data.setValueTextSize(13.5f)
+        data.setValueTextColor(Color.WHITE)
 
         binding.pieChart.data = data
         binding.pieChart.invalidate()
+        binding.pieChart.isRotationEnabled = true
 
-
+        //update the info
         if (user != null) {
             binding.textName.text = user.displayName
             binding.textEmail.text = user.email
