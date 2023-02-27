cbd5adcea533c97be47bce46c094715cca22d31e
armaan <armaanbadhan07@gmail.com>
2023-02-27 15:54:32 +0530

    star added

--- app/src/main/java/com/pec_acm/moviedroid/mainpage/detail/TvDetailFragment.kt
+++ app/src/main/java/com/pec_acm/moviedroid/mainpage/detail/TvDetailFragment.kt
@@ -39,11 +39,7 @@ class TvDetailFragment : Fragment() {
                 Glide.with(this).load("https://image.tmdb.org/t/p/w780" + tvDetail.backdrop_path)
                     .into(binding.image)
             }
-            binding.rating.text = buildString {
-                append("Rated: ")
-                append(String.format("%.2f", tvDetail.vote_average))
-                append("/10")
-            }
+            binding.rating.text = String.format("%.2f", tvDetail.vote_average)
             var genres = ""
             for (i in tvDetail.genres) {
                 genres += i.name + "  "
